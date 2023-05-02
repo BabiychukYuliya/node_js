32a66e51a9f407abead6499fbf01406ba51625bc
BabiychukYuliya <julia4233097@gmail.com>
2023-05-02 23:14:45 +0300

    add commander

---
+++ contacts.js
@@ -0,0 +1,49 @@
+const fs = require('fs/promises');
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname,'db/contacts.json' );
+
+
+// TODO: задокументувати кожну функцію
+const listContacts = async () => {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+}
+
+const getContactById = async (id) => {
+    const allContacts = await listContacts();
+    const data = allContacts.find(item => item.id === id);
+    return data || null;
+}
+
+const removeContact = async (id) => {
+    const allContacts = await listContacts();
+    const index = allContacts.findIndex(item => item.id === id);
+    if (index === -1) {
+        return null;
+    }
+    const [result] = allContacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return result;
+}
+
+const addContact = async (name, email, phone) => {
+    const contacts = await listContacts();
+    const newContact = {
+        id: nanoid(),
+        name,
+        email,
+        phone
+    }
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    addContact, 
+    removeContact
+}
